c5ff793bfb822ed9d4472f8fa32f23e2781387b1
aditya <adityavittal92@gmail.com>
2024-11-16 12:34:25 +0530

    update
    update

--- Expense-v1/expensev1.tf
+++ Expense-v1/expensev1.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "ec2-nodes" {
-  count = length(var.node-names)
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.small"
+  count                  = length(var.components)
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t2.small"
   vpc_security_group_ids = ["sg-064a6677c666b0183"]
   tags = {
-    Name = "${var.node-names[count.index]}-dev"
+    Name = var.components[count.index]
   }
 }
 
 
-variable "node-names" {
+variable "components" {
   default = ["frontend", "backend", "mysql"]
 }
