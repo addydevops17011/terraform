abe661c4668165e84d9bd3d98f3e0f9876be2039
aditya <adityavittal92@gmail.com>
2024-11-16 20:20:44 +0530

    update
    update

--- Expense-v1/app/main.tf
+++ Expense-v1/app/main.tf
@@ -19,7 +19,7 @@ resource "aws_route53_record" "record" {
 
 resource "null_resource" "provisioner" {
   depends_on = [aws_route53_record.record]
-  provisioner "local-exec" { # Using remote-exec instead of local-exec
+  provisioner "remote-exec" { # Using remote-exec instead of local-exec
     command = "sleep 120; cd /home/ec2-user/ansible-expense ; ansible-playbook -i ${aws_instance.ec2-node.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env_name=dev expense.yml"
   }
 }
