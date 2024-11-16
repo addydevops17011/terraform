ab22600d8f33b024f0fd1812af330fd5c554e9ba
aditya <adityavittal92@gmail.com>
2024-11-16 11:58:30 +0530

    update
    update

--- Expense-v1/expensev1.tf
+++ Expense-v1/expensev1.tf
@@ -1,23 +1,10 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.small"
+  vpc_security_group_ids = ["sg-064a6677c666b0183"]
   tags = {
     Name = "frontend-dev"
   }
 }
 
-resource "aws_instance" "backend" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.small"
-  tags = {
-    Name = "backend-dev"
-  }
-}
 
-resource "aws_instance" "mysql" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.small"
-  tags = {
-    Name = "mysql-dev"
-  }
-}
