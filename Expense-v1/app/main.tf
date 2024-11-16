297099eb1eaf9e9c8d4f6113861af35bf4606388
aditya <adityavittal92@gmail.com>
2024-11-16 20:04:34 +0530

    update
    update

--- Expense-v1/app/main.tf
+++ Expense-v1/app/main.tf
@@ -5,6 +5,17 @@ resource "aws_instance" "ec2-node" {
   tags = {
     Name = var.name
   }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password    = "DevOps321"
+    host        = self.public_ip # Using public_ip instead of private_ip
+  }
+
+  provisioner "local-exec" { # Using remote-exec instead of local-exec
+    command = "sleep 120; cd /home/ec2-user/ansible-expense; ansible-playbook -i ${self.private_ip} -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env_name=dev expense.yml"
+  }
 }
 
 resource "aws_route53_record" "record" {
@@ -12,7 +23,5 @@ resource "aws_route53_record" "record" {
   name    = "${var.name}.doubtfree.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.ec2-node.private_ip]
+  records = [aws_instance.ec2-node.public_ip] # Using public_ip instead of private_ip
 }
-
-
