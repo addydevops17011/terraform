c6240c35123addbaa33cf224eabe89d8d1b76a72
aditya <adityavittal92@gmail.com>
2024-11-16 13:05:05 +0530

    update
    update

--- Expense-v1/expensev1.tf
+++ Expense-v1/expensev1.tf
@@ -1,14 +1,23 @@
 resource "aws_instance" "ec2-nodes" {
-  count                  = length(var.components)
-  ami                    = "ami-09c813fb71547fc4f"
-  instance_type          = "t2.small"
+  for_each = var.components
+  ami      = "ami-09c813fb71547fc4f"
+  #check a values exits in map or not
+  instance_type          = each.value.instance_type == null ? "t2.micro" : each.value.instance_type
   vpc_security_group_ids = ["sg-064a6677c666b0183"]
   tags = {
-    Name = var.components[count.index]
+    Name = each.key
   }
 }
 
-
 variable "components" {
-  default = ["frontend", "backend", "mysql"]
+  default = {
+    frontend = {
+
+    }
+    backend = {
+    }
+    mysql = {
+      instance_type = "t2.micro"
+    }
+  }
 }
