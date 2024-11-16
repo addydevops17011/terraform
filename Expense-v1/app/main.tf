44601c0454d4065310d0a2a41ec5eac4f492da75
aditya <adityavittal92@gmail.com>
2024-11-16 20:12:16 +0530

    update
    update

--- Expense-v1/app/main.tf
+++ Expense-v1/app/main.tf
@@ -6,16 +6,7 @@ resource "aws_instance" "ec2-node" {
     Name = var.name
   }
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    password    = "DevOps321"
-    host        = self.public_ip # Using public_ip instead of private_ip
-  }
 
-  provisioner "local-exec" { # Using remote-exec instead of local-exec
-    command = "sleep 120; cd /home/ec2-user/ansible-expense; ansible-playbook -i ${self.private_ip} -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env_name=dev expense.yml"
-  }
 }
 
 resource "aws_route53_record" "record" {
@@ -23,5 +14,12 @@ resource "aws_route53_record" "record" {
   name    = "${var.name}.doubtfree.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.ec2-node.public_ip] # Using public_ip instead of private_ip
+  records = [aws_instance.ec2-node.private_ip] # Using public_ip instead of private_ip
+}
+
+resource "null_resource" "provisioner" {
+  depends_on = [aws_route53_record.record]
+  provisioner "local-exec" { # Using remote-exec instead of local-exec
+    command = "sleep 120; cd /home/ec2-user/ansible-expense; ansible-playbook -i ${aws_instance.ec2-node.private_ip} -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env_name=dev expense.yml"
+  }
 }
