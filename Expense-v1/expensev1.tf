7314859cabff330fbf3e4179e9d5e1ae3952824d
aditya <adityavittal92@gmail.com>
2024-11-16 12:30:53 +0530

    update
    update

--- Expense-v1/expensev1.tf
+++ Expense-v1/expensev1.tf
@@ -1,27 +1,14 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "ec2-nodes" {
+  count = length(var.node-names)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.small"
   vpc_security_group_ids = ["sg-064a6677c666b0183"]
   tags = {
-    Name = "frontend-dev"
-  }
-}
-
-resource "aws_instance" "backend" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.small"
-  vpc_security_group_ids = ["sg-064a6677c666b0183"]
-  tags = {
-    Name = "backend-dev"
+    Name = "${var.node-names[count.index]}-dev"
   }
 }
 
 
-resource "aws_instance" "mysql" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.small"
-  vpc_security_group_ids = ["sg-064a6677c666b0183"]
-  tags = {
-    Name = "mysql-dev"
-  }
+variable "node-names" {
+  default = ["frontend", "backend", "mysql"]
 }
